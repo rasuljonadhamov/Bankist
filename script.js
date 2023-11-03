be921305f086bf3a039b93ec4b35682b7451408b
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-11-03 17:45:38 +0300

    computing usernames

--- script.js
+++ script.js
@@ -80,6 +80,19 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const createUserNames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.userName = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUserNames(accounts);
+console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
